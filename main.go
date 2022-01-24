5d5fd75282fd7101e77ccb80f8aaac4cbb6f509b
lixin yang <2690688423@qq.com>
2022-01-24 16:10:49 +0800

    add main

--- main.go
+++ main.go
@@ -4,15 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Person struct {
+	XMLName	xml.Name	`xml:"person"`
+	FirstName string	`xml:"firstName,attr"`
+	LastName string	`xml:"lastName,attr"`
+}
+
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello-world",
-		})
-	})
+	router.GET("/", IndexHandler)
 
-	router.Run(":5000")
+	router.Run()
 
 } 
+
+func IndexHandler(c *gin.Context)  {
+	c.XML(200, Person{
+		FirstName: "Lixv",
+		LastName:	"Yang",
+	})
+}
