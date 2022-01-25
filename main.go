7e2200efa0c7d359e20a947bf3f0f72f2d2fedce
lixin yang <2690688423@qq.com>
2022-01-25 21:19:23 +0800

    added mongodb integration

--- main.go
+++ main.go
@@ -17,27 +17,37 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	// "encoding/json"
+	// "io/ioutil"
 	"net/http"
 	"strings"
 	"time"
+	"context"
+	"os"
+	"log"
+
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/mongo"
+ 	"go.mongodb.org/mongo-driver/mongo/options"
+ 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var recipes []Recipe
-
+var ctx context.Context
+var err error
+var client *mongo.Client
 // swagger:parameters recipes newRecipe
 type Recipe struct {
 	//swagger:ignore
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	ID primitive.ObjectID `json:"id" bson:"_id"`
+	Name string `json:"name" bson:"name"`
+	Tags []string `json:"tags" bson:"tags"`
+	Ingredients []string `json:"ingredients" bson:"ingredients"`
+	Instructions []string `json:"instructions" bson:"instructions"`
+	PublishedAt time.Time `json:"publishedAt"
+	bson:"publishedAt"`
 }
 
 // swagger:operation GET /recipes recipes listRecipes
@@ -49,6 +59,20 @@ type Recipe struct {
 //     '200':
 //         description: Successful operation
 func ListRecipesHandler(c *gin.Context) {
+	cur, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,
+		gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(ctx)
+	
+	recipes := make([]Recipe, 0)
+	for cur.Next(ctx) {
+		var recipe Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
 	c.JSON(http.StatusOK, recipes)
 }
 
@@ -69,10 +93,16 @@ func NewRecipeHandler(c *gin.Context) {
 		return
 	}
 
-	recipe.ID = xid.New().String()
+	recipe.ID = primitive.NewObjectID()
 	recipe.PublishedAt = time.Now()
-
-	recipes = append(recipes, recipe)
+	
+	_, err = collection.InsertOne(ctx, recipe)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError,
+		gin.H{"error": "Error while inserting a new recipe"})
+		return
+	}
 
 	c.JSON(http.StatusOK, recipe)
 }
@@ -103,22 +133,22 @@ func UpdateRecipeHandler(c *gin.Context) {
 		return
 	}
 
-	index := -1
-	for i := 0; i < len(recipes); i++ {
-		if recipes[i].ID == id {
-			index = i 
-			break
-		}
-	}
-
-	if index == -1 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err = collection.UpdateOne(ctx, bson.M{
+			"_id": objectId,
+	}, bson.D{{"$set", bson.D{
+			{"name", recipe.Name},
+			{"instructions", recipe.Instructions},
+			{"ingredients", recipe.Ingredients},
+			{"tags", recipe.Tags},
+	}}})
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
 		return
 	}
-
-	recipes[index] = recipe
-
-	c.JSON(http.StatusOK, recipe)
+	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been updated"})
 }
 
 // swagger:operation DELETE /recipes/{id} recipes deleteRecipe
@@ -220,9 +250,30 @@ func GetRecipeHandler(c *gin.Context) {
 }
 
 func init() {
-	recipes = make([]Recipe, 0)
-	file, _ := ioutil.ReadFile("recipes.json")
-	_ = json.Unmarshal([]byte(file), &recipes)
+	// recipes = make([]Recipe, 0)
+ 	// file, _ := ioutil.ReadFile("recipes.json")
+ 	// _ = json.Unmarshal([]byte(file), &recipes)
+ 	ctx = context.Background()
+	client, err = mongo.Connect(ctx,
+	options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err = client.Ping(context.TODO(),
+		readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+	// var listOfRecipes []interface{}
+	// for _, recipe := range recipes {
+	// listOfRecipes = append(listOfRecipes, recipe)
+	// }
+	// collection := client.Database(os.Getenv(
+	// "MONGO_DATABASE")).Collection("recipes")
+	// insertManyResult, err := collection.InsertMany(
+	// ctx, listOfRecipes)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println("Inserted recipes: ",
+	// len(insertManyResult.InsertedIDs))
 }
 
 func main() {
